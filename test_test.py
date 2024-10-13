449c2b8542fbbeb72696563e2581179554e2cada
ramezani13 <147407440+ramezani13@users.noreply.github.com>
2024-10-13 18:30:40 +0200

    test_test.py

--- test_test.py
+++ test_test.py
@@ -1,5 +1,17 @@
-import subprocess
+from check_word import contains_word
 
-def test_hello_world():
-    result = subprocess.run(['python3', 'main.py'], stdout=subprocess.PIPE)
-    assert result.stdout.decode('utf-8').strip() == "Hello, World!"
+def test_contains_word1():
+    assert contains_word("The quick brown fox", "quick") == True
+
+def test_contains_word2():
+    assert contains_word("The quick brown fox", "slow") == False
+
+def test_contains_word3():
+    assert contains_word("Hello world", "world") == True
+
+def test_contains_word4():
+    assert contains_word("Hello world", "earth") == False
+
+def test_contains_word5():
+    assert contains_word("Python is fun", "Python") == True
+
